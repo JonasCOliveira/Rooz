a51784bbe157d8ffce91f1170b67bea230a8ef0d
JonasCOliveira <jonascauan@outlook.com>
2020-11-30 12:23:24 -0300

    Game commit

---
+++ Rooz/Assets/scripts/PlayerHealth.cs
@@ -0,0 +1,39 @@
+﻿
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+
+    public static Animator anim;
+
+    void Start(){
+
+        anim = gameObject.GetComponent<Animator>();
+
+    }
+
+    public static void Reset(){
+
+        
+        if(GameMaster.instance.GetNumOfHearts() == 0){
+            Debug.Log("Reset");
+           SceneManager.LoadScene(SceneManager.GetActiveScene().name); 
+        } 
+
+        else {
+             Debug.Log("Checkpoint");
+             PlayerPos.LoadCheckPoint();
+             GameMaster.instance.AttHud();
+
+        }
+
+    }
+
+    public static void Hurt(){
+
+        anim.SetBool("Hurting", true);
+
+    }
+}
---
+++ Rooz/Assets/scripts/PlayerScore.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerScore : MonoBehaviour
+{
+
+     void OnTriggerEnter2D(Collider2D other){
+
+	if(other.CompareTag("coins")){
+
+            GameMaster.instance.SetNumOfPoints(1);
+        	GameMaster.instance.AttHud();
+    }
+ 
+    }
+
+}
